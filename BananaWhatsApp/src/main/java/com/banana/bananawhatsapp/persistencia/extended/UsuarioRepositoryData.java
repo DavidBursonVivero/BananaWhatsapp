159f897ef16b45cb6ff377311f6890ddb37fd845
DavidB <davibursonvivero.db@gmail.com>
2024-02-12 16:26:19 +0100

    query

--- BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/persistencia/extended/UsuarioRepositoryData.java
+++ BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/persistencia/extended/UsuarioRepositoryData.java
@@ -1,5 +1,6 @@
 package com.banana.bananawhatsapp.persistencia.extended;
 
+import com.banana.bananawhatsapp.modelos.Mensaje;
 import com.banana.bananawhatsapp.modelos.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,12 +14,11 @@ public interface UsuarioRepositoryData extends JpaRepository <Usuario, Integer> 
     public Set<Usuario> findByNombreContains(String nombre);
     public Usuario findByMensaje_Remitente(String remitente);*/
 
-    @Query("SELECT m * FROM mensaje u JOIN mensaje m on (m.from_user = u.id) WHERE m.id  LIKE %?1")
-    public List <String> findRemitenteByMensajeId (Integer id);
+    @Query("SELECT m.* FROM mensaje m WHERE m.FROM_user LIKE ?1 orm.to_user LIKE ?1")
+    public List <Mensaje> allMensajeByUserID (Integer id);
 
     @Query("SELECT u.nombre From usuario u JOIN usuario_mensaje um on (um.usuario_id = u.id) JOIN mensaje m on (um.mensaje_id = m.id) WHERE m.id LIKE %?1")
     public List <String> findDestinatarioByMensajeId (Integer id);
-
 
 
 
